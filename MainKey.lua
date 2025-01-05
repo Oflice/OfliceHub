00405d496c01f439b58e8d7bcb1ddebd7d67c315
Oflice <ofliceofficial@gmail.com>
2025-01-05 11:47:27 +0200

    Update MainKey.lua

--- MainKey.lua
+++ MainKey.lua
@@ -27,66 +27,10 @@ local btn = Tab:AddButton({
             OrionLib:Destroy()
             local placeeeif = game.PlaceId
                 
-            if game.PlaceId == 90192874177359 then
-                local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
-
-local Window = OrionLib:MakeWindow({Name = "Ottd Script", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
-
-local Tab = Window:MakeTab({
-	Name = "Spawn",
-	Icon = "",
-	PremiumOnly = false
-})
-
-local towers = {
-    "Engineer Cameraman",
-    "Large Scientist Cameraman",
-    "Scientist Cameraman",
-    "Titan Cameraman",
-    "TV Woman",
-    "Jetpack Cameraman",
-    "Elf Cameraman",
-    "Titan Presentwoman",
-    "Speaker Helicopter",
-    "Large Cameraman",
-    "Laser Cameraman Car",
-    "Upgraded Titan Cameraman",
-    "Upgraded Titan Speakerman",
-    "Titan Cinemaman",
-    "Ninja Cameraman",
-    "Large TV Man",
-    "Large Speakerman",
-    "Car Speakerman",
-    "Camerawoman",
-    "Speakerman"
-}
-
-for i, v in ipairs(towers) do
-   Tab:AddButton({
-       Name = v,
-       Callback = function()
-           local player = game.Players.LocalPlayer
-           local character = player.Character or player.CharacterAdded:Wait()
-           local position = character:WaitForChild("HumanoidRootPart").CFrame
-
-           local args1 = {
-               [1] = v
-           }
-           game:GetService("ReplicatedStorage").Functions.RequestTower:InvokeServer(unpack(args1))
-
-           local args2 = {
-               [1] = v,
-               [2] = position
-           }
-           game:GetService("ReplicatedStorage").Functions.ChangeMode:InvokeServer(unpack(args2))
-       end
-   })
-end
-
-OrionLib:Init()
+            if game.PlaceId == "90192874177359" then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Oflice/OfliceHub/refs/heads/main/Scripts/oldttdgame.lua", true))()
              end
-        end    
-    end    
+        end      
 })
 
 OrionLib:Init()
