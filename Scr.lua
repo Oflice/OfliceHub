d2581baf9d48c737627ae37af20219c10f0cc43b
Oflice <ofliceofficial@gmail.com>
2025-10-15 12:54:16 +0300

    Update Scr.lua

--- Scr.lua
+++ Scr.lua
@@ -1,3 +1,4 @@
 return {
     [90192874177359] = "https://raw.githubusercontent.com/Oflice/OfliceHub/refs/heads/main/Scripts/oldttdgame.lua",
+    [81351013934902] = "https://raw.githubusercontent.com/Oflice/OfliceHub/refs/heads/main/Scripts/ogttd.lua",
 }
